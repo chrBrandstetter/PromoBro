fa3c928cbad42643f385f523ac0f67a55f5722bb
chrBrandstetter <christoph.br4ndstetter@gmail.com>
2023-11-25 19:19:24 +0100

    Init

---
+++ PromoBro/PromoBro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PromoBro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PromoBro/PromoBro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PromoBro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
