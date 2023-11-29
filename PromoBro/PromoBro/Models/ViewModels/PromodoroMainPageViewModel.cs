65b6ef4e961537ed287fda43b81297fce2cef3f4
chrBrandstetter <christoph.br4ndstetter@gmail.com>
2023-11-29 20:23:00 +0100

    set up MVVM
    implemented INotifyPropertyChanged

---
+++ PromoBro/PromoBro/Models/ViewModels/PromodoroMainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoBro.Models.ViewModels
+{
+    public class PromodoroMainPageViewModel : INotifyPropertyChanged
+    {
+        private int _remainingTime;
+        public int RemainingTime 
+        {
+            get 
+            { 
+                return _remainingTime; 
+            }
+            set
+            {
+                _remainingTime = value;
+                OnPropertyChanged(nameof(RemainingTime));
+            }
+        }
+
+        public string Description { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- PromoBro/PromoBro/Views/PromodoroMainPage.xaml.cs
+++ PromoBro/PromoBro/Views/PromodoroMainPage.xaml.cs
@@ -1,15 +1,27 @@
+using Android.Media;
+using PromoBro.Models.ViewModels;
+
 namespace PromoBro.Views;
 
 public partial class PromodoroMainPage : ContentPage
 {
-	public PromodoroMainPage()
-	{
-		InitializeComponent();
-	}
+    public PromodoroMainPageViewModel promodoroMainPageViewViewModel { get; set; }
+    public PromodoroMainPage()
+    {
+        InitializeComponent();
+        promodoroMainPageViewViewModel = new PromodoroMainPageViewModel(); 
+        BindingContext = promodoroMainPageViewViewModel; //Bind the data of the ViewModel
+    }
+    
 
     private void btnHistory_Clicked(object sender, EventArgs e)
     {
-		//navigate to corresponding Page
-		Shell.Current.GoToAsync(nameof(PromodoroHistoryPage));
+        //navigate to corresponding Page
+        Shell.Current.GoToAsync(nameof(PromodoroHistoryPage));
+    }
+
+    private void btnTimer_Clicked(object sender, EventArgs e)
+    {
+        promodoroMainPageViewViewModel.RemainingTime = 1; 
     }
 }
